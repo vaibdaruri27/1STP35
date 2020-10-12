a7bce8189f76a71ae65cd1d77c724beb8b4aa1db
vaibdaruri27 <vaibdaruri@gmail.com>
2020-10-12 18:38:46 -0400

    Project-34 FINAL

--- sketch.js
+++ sketch.js
@@ -1,21 +1,64 @@
 //Create variables here
+var dog1, dog2;
+var dog, happyDog, database, foodS, foodStock;
+
 
 function preload()
 {
-	//load images here
+  dog1 = loadImage("images/dogImg.png");
+  dog2 = loadImage("images/dogImg1.png");
 }
 
 function setup() {
-	createCanvas(800, 700);
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,250,20,20);
+  dog.addImage(dog1);
+  dog.scale = 0.3;
+
+  happyDog = createSprite(300,300,30,30);
+
+  database = firebase.database();
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", function readStock1(data){
+     foodS = data.val();
+  })
   
 }
 
 
 function draw() {  
+  background(rgb(46,139,87));
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.visible = false;
+    happyDog.addImage(dog2);
+    happyDog.scale = 0.3;
+  }
 
   drawSprites();
-  //add styles here
+  
+  textFont("Arial Black")
+  textSize(15);
+  fill("blue");
+  text("Note: Press UP_ARROW Key to feed your very own...DOG!", 20,40);
 
+  //add styles here
+  
+}
+
+function writeStock(x){
+   if(x<=0){
+     x=0;
+   }else{
+     x = x-1;
+   }
+
+   database.ref('/').update({
+     Food:x
+   })
 }
 
 
